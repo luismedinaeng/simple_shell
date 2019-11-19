bdb3411a1303b7cb929f6ee14fd58a5cec23de63
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-19 23:23:45 +0000

    [Add] header file

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef S_SHELL
+#define S_SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+char **str_tok(char *command, ssize_t r_b);
+
+#endif
