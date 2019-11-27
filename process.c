6b89a196bbfd241fd52bb1f6da5c790b94b7b64f
luismedinaeng <malf95@hotmail.com>
2019-11-27 18:25:45 -0500

    [Update]Change standard functions for custom ones

--- process.c
+++ process.c
@@ -1,14 +1,24 @@
 #include "holberton.h"
-void *handle_error(char *comm, int _stat);
+void handle_error(char *comm, int _stat);
 int print_number(int n);
 
+/**
+ * str_process - Process and executes a command
+ * @command: Command to execute
+ * @b_r: Count of bytes inside commnad parameter
+ * @c: Count of the command
+ *
+ * Return: Null pointers
+ */
 char **str_process(char **command, ssize_t b_r, int c)
 {
 	pid_t _pid;
 	char *tkn, *tkns;
 	char **input_bu;
 	int status, exe, n = 0, ex_it = 0, i = 0;
-	char **input = malloc(2 * sizeof(char*));
+	int s_m = (2 * sizeof(char *));
+	char **input = malloc(2 * sizeof(char *));
+
 	if (input == NULL)
 		return (NULL);
 
@@ -18,17 +28,18 @@ char **str_process(char **command, ssize_t b_r, int c)
 	tkn = strtok(*command, " \n\t");
 	if (tkn == NULL)
 	{
-		free (input);
+		free(input);
 		return (NULL);
 	}
 	n = 0;
 	while (tkn != NULL)
 	{
-		if(n >= 1)
+		if (n >= 1)
 		{
-			input_bu = realloc(input, (n + 2) * sizeof(char*));
+			input_bu = (char **)_realloc((char *)input, s_m, (n + 2) * sizeof(char *));
 			if (input_bu == NULL)
-				return NULL;
+				return (NULL);
+			s_m = ((n + 2) * sizeof(char *));
 			input = input_bu;
 		}
 		input[n] = tkn;
@@ -43,15 +54,15 @@ char **str_process(char **command, ssize_t b_r, int c)
 		if (ex_it < 0)
 		{
 			write(STDERR_FILENO, "./shell: 1: exit: Illegal number: ", 34);
-			write(STDERR_FILENO, input[1], strlen(input[1]));
+			write(STDERR_FILENO, input[1], _strlen(input[1]));
 			write(STDERR_FILENO, "\n", 1);
-			return NULL;
+			return (NULL);
 		}
 		if (ex_it > 255)
 		{
 			exit((ex_it % 256));
 		}
-		else if (strcmp(input[0], "exit") == 0)
+		else if (_strcmp(input[0], "exit") == 0)
 		{
 			free(input), free(*command);
 			exit(ex_it);
@@ -59,7 +70,7 @@ char **str_process(char **command, ssize_t b_r, int c)
 	}
 	else
 	{
-		if(strcmp(input[0], "exit") == 0)
+		if (_strcmp(input[0], "exit") == 0)
 		{
 			free(input), free(*command);
 			exit(ex_it);
@@ -69,7 +80,7 @@ char **str_process(char **command, ssize_t b_r, int c)
 	_pid = fork();
 	if (_pid == 0)
 	{
-		exe = execve(input[0], input, NULL);
+		exe = execve(input[0], input, environ);
 		if (exe == -1)
 		{
 			handle_error(input[0], c);
@@ -86,25 +97,37 @@ char **str_process(char **command, ssize_t b_r, int c)
 	return (NULL);
 }
 
-void *handle_error(char *comm, int _stat)
+/**
+ * handle_error - Function that prints the info message of an error
+ * @comm: Command which refers the error
+ * @_stat: number of the command
+ *
+ * Return: Nothing;
+ */
+void handle_error(char *comm, int _stat)
 {
-
 	write(STDERR_FILENO, "./shell: ", 9);
 	print_number(_stat);
 	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, comm, strlen(comm));
+	write(STDERR_FILENO, comm, _strlen(comm));
 	write(STDERR_FILENO, ": not found\n", 12);
-	return (NULL);
 }
 
+/**
+ * print_number - Prints a number on the standard output
+ * @n: Number to print
+ *
+ * Return: The number printed
+ */
 int print_number(int n)
 {
 	char conv;
+
 	if (n / 10)
 	{
 		print_number(n / 10);
 	}
 	conv = ((n % 10) + '0');
 	write(STDERR_FILENO, &conv, 1);
-	return (0);
+	return (n);
 }
--- prompt.c
+++ prompt.c
@@ -1,43 +1,48 @@
 #include "holberton.h"
 
+/**
+ * sighandler - Function that handles a signal
+ * @sig: Code of signal
+ *
+ * Return: Nothing
+ */
 void sighandler(int sig)
 {
 	(void) sig;
 	write(STDOUT_FILENO, "\n$ ", 3);
 }
 
+/**
+ * main - Entry point of application that emulates a simple shell
+ * @argc: Count of arguments
+ * @argv: Vector of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	ssize_t bytes_read;
-	size_t num_bytes;
+	size_t num_bytes = 0;
 	pid_t _pid;
-	char *str, *p_sign = "$ ";
-	char **intro;
-	int i = 0, count = 1;
+	char *str = NULL, *p_sign = "$ ", **intro;
+	int count = 1;
 
-	(void) i;
 	(void)intro;
 	(void)_pid;
-	num_bytes = 0;
-	str = NULL;
-
 	if (argc > 1)
 	{
-		write(STDERR_FILENO, argv[0], strlen(argv[0]));
+		write(STDERR_FILENO, argv[0], _strlen(argv[0]));
 		write(STDERR_FILENO, ": 0: ", 5);
 		write(STDERR_FILENO, "Can't open ", 11);
-		write(STDERR_FILENO, argv[1], strlen(argv[1]));
+		write(STDERR_FILENO, argv[1], _strlen(argv[1]));
 		write(STDERR_FILENO, "\n", 1);
 		exit(127);
 	}
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, p_sign, strlen(p_sign));
-
+			write(STDOUT_FILENO, p_sign, _strlen(p_sign));
 		signal(SIGINT, sighandler);
-
 		bytes_read = getline(&str, &num_bytes, stdin);
 		if (bytes_read == EOF)
 		{
@@ -47,16 +52,13 @@ int main(int argc, char *argv[])
 			exit(0);
 		}
 		if (bytes_read == -1)
-		{
 			exit(-1);
-		}
 		else
 		{
 			intro = str_process(&str, bytes_read, count);
 			free(str);
 		}
-		str = NULL;
-		count++;
-		free(str);
+		str = NULL, count++, free(str);
 	}
+	return (0);
 }
