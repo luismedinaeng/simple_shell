2d4a980f8abc32d37f14a5d60d0bc5ea5e41fc3a
luismedinaeng <malf95@hotmail.com>
2019-11-27 04:59:39 -0500

    [Add]str_concat function

--- auxfun_2.c
+++ auxfun_2.c
@@ -24,3 +24,31 @@ char *_strchr(char *s, char c)
 
 	return (0);
 }
+
+/**
+ * str_concat - Concatenates two strings
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return: Pointer to the array created with malloc
+ * Null if falls, or str is Null
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int len1, len2, len;
+
+	len1 = s1 == NULL ? 0 : _strlen(s1);
+	len2 = s2 == NULL ? 0 : _strlen(s2);
+	len = len1 + len2;
+
+	s = (char *)malloc((len + 1) * sizeof(char));
+
+	if (s != NULL)
+	{
+		s = _strcpy(s, (s1 == NULL ? "\0" : s1));
+		s = _strcpy(s + len1, (s2 == NULL ? "\0" : s2));
+		s -= len1;
+	}
+	return (s);
+}
