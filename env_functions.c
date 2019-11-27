a4273528836edeb04af1e67842f34e19d5d32523
luismedinaeng <malf95@hotmail.com>
2019-11-27 05:00:27 -0500

    [Update]Deletes global variables and modifying it corresponding functions

--- env_functions.c
+++ env_functions.c
@@ -3,11 +3,12 @@
 /**
  * _genenv - Creates a copy of the environ variable to a new global variable
  *
- * Return: A double pointer variable that has the information of the environment
+ * Return: A double pointer variable that has the information of the
+ * environment variables
  */
 char **_genenv(void)
 {
-	extern char **environ;
+	char **env_var;
 	int i = 0, count = 0;
 
 	while (environ && environ[count])
@@ -22,7 +23,7 @@ char **_genenv(void)
 		env_var[i] = _strdup(environ[i]);
 		if (env_var[i] == NULL)
 		{
-			_free_env();
+			_free_env(&env_var);
 			return (NULL);
 		}
 		i++;
@@ -34,29 +35,31 @@ char **_genenv(void)
 
 /**
  * _free_env - Frees the copy generated for environmental variables
+ * @env_var: The avriable of environmental variables to free
  *
  * Return: Nothing
  */
-void _free_env(void)
+void _free_env(char ***env_var)
 {
 	int i = 0;
 
-	while (env_var && env_var[i])
+	while (*env_var && (*env_var)[i])
 	{
-		free(env_var[i]);
-		env_var[i] = NULL;
+		free((*env_var)[i]);
+		(*env_var)[i] = NULL;
 		i++;
 	}
-	free(env_var);
-	env_var = NULL;
+	free(*env_var);
+	*env_var = NULL;
 }
 
 /**
  * _printenv - Prints the environmental variables
+ * @env_var: Environamental variables to print
  *
  * Return: Nothing
  */
-void _printenv(void)
+void _printenv(char **env_var)
 {
 	char buffer[1024];
 	int i, j, count;
@@ -88,12 +91,13 @@ void _printenv(void)
 
 /**
  * _getenv - Gets an environmental variable from the copy
+ * @env_var: Environmental variables where to find the required one
  * @name: Name of the variable to get
  *
  * Return: The value of the variable to get created with malloc
  * NULL if the variable does not exist
  */
-char *_getenv(char *name)
+char *_getenv(char **env_var, char *name)
 {
 	unsigned int i = 0;
 	char *strcp;
@@ -102,7 +106,7 @@ char *_getenv(char *name)
 	{
 		strcp = _strstr(env_var[i], name);
 		if (strcp != NULL && strcp == env_var[i])
-				return (_strchr(env_var[i], '=') + 1);
+			return (_strchr(env_var[i], '=') + 1);
 		i++;
 	}
 	return (NULL);
--- path.h
+++ path.h
@@ -6,34 +6,40 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
- * struct env_var - Struct of a singly linked list to manage the strings
+ * struct env_path - Struct of a singly linked list to manage the strings
  * @value: Value of the actual node
  * @next: Next item on the sinlgy linked list
  */
 typedef struct env_path
 {
 	char *value;
-	struct env_var *next;
+	struct env_path *next;
 } env_path_t;
 
+/* Variable that imports the info of the environment */
+extern char **environ;
+
 /* Variable that has the environmental variables information */
-char **env_var;
+/* char **env_var; */
 
 /* Functions to manage environmental variables */
 char **_genenv(void);
-void _free_env(void);
-void _printenv(void);
-char *_getenv(char *name);
+void _free_env(char ***env_var);
+void _printenv(char **env_var);
+char *_getenv(char **env_var, char *name);
 int _setenv(const char *name, const char *value, int overwrite);
 int _unsetenv(const char *name);
 
 /* Functions to work with the env var PATH */
-env_path_t *build_path(void);
+env_path_t *build_path(char **env_var);
 env_path_t *add_path_end(env_path_t **head, const char *n);
 void free_path(env_path_t **head);
 size_t print_path(const env_path_t *h);
+char *check_file(env_path_t *head, char *fl);
 
 /* Auxiliar functions of strings */
 char *_strdup(char *str);
@@ -41,8 +47,8 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strstr(char *haystack, char *needle);
-char *_strchr(char *s, char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
 
+char *_strchr(char *s, char c);
+char *str_concat(char *s1, char *s2);
 
 #endif
